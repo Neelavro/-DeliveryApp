85ea26a8d7e113962a8f7c57566d6e2dc24548cd
TashfiaSaad <tashfiasaad9@gmail.com>
2024-06-21 15:23:22 +0600

    commit

---
+++ lib/screens/MyHomePage.dart
@@ -0,0 +1,66 @@
+import "package:flutter/material.dart";
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter=0;
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.white70,
+        title: Text(
+          'Hi, I am Tashfia.',
+          style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.w900,
+            color: Colors.black12
+
+          )
+        )
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Press the button to increase.",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black26
+              )
+            ),
+            Text(
+                "${counter}",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black26
+                )
+
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          counter+=1;
+          print(counter);
+          setState(() {
+          });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 20,
+        ),
+      ),
+    );
+  }
+}
