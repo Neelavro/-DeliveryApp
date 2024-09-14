85f5fe61a87c662572b3635ec89c6534e6857a4d
TashfiaSaad <tashfiasaad9@gmail.com>
2024-09-14 14:51:30 +0600

    1. order api created

--- backend/models.py
+++ backend/models.py
@@ -23,8 +23,8 @@ class Restaurant(models.Model):
 class Menu(models.Model):  # Renamed from 'menu' to 'Menu' (Class names should be capitalized)
     restaurant = models.ForeignKey(Restaurant, related_name='menu_items', on_delete=models.CASCADE)
     item_name = models.CharField(max_length=50, null=False)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    item_description = models.CharField(max_length=100, null=True)
+    item_price = models.DecimalField(max_digits=10, decimal_places=2, null = True)
+    item_description = models.CharField(max_length=100, null = True)
 
     def __str__(self):
         return self.item_name
